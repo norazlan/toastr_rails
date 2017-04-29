50e72ccaea68aebbf37eea2fc5edb3b1b65a3ac9
norazlan <norazlan@users.noreply.github.com>
2017-04-29 16:28:32 +0800

    update options

--- app/assets/javascript/toastr_rails.js
+++ app/assets/javascript/toastr_rails.js
@@ -4,8 +4,11 @@
 toastr.options = {
   "closeButton": true,
   "debug": false,
+  "newestOnTop": true,
   "progressBar": true,
-  "positionClass": "toast-top-right",
+  "positionClass": "toast-top-full-width",
+  "preventDuplicates": true,
+  "onclick": null,
   "showDuration": "300",
   "hideDuration": "1000",
   "timeOut": "5000",
